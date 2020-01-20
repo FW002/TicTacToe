dec198ef4d7295400a808c0a7d0a20bb7fef29a7
Fabian Weißensteiner <fabian.weissensteiner@gmx.at>
2020-01-20 13:40:22 +0100

    VS_File
    The VS File

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.DebugL = new System.Windows.Forms.Label();
+            this.ResetState = new System.Windows.Forms.Button();
+            this.PlayerL = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel.Location = new System.Drawing.Point(184, 92);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(357, 307);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPanelPaint);
+            this.panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+            // 
+            // DebugL
+            // 
+            this.DebugL.AutoSize = true;
+            this.DebugL.Location = new System.Drawing.Point(323, 414);
+            this.DebugL.Name = "DebugL";
+            this.DebugL.Size = new System.Drawing.Size(50, 17);
+            this.DebugL.TabIndex = 1;
+            this.DebugL.Text = "Coord.";
+            // 
+            // ResetState
+            // 
+            this.ResetState.Location = new System.Drawing.Point(13, 92);
+            this.ResetState.Name = "ResetState";
+            this.ResetState.Size = new System.Drawing.Size(75, 23);
+            this.ResetState.TabIndex = 2;
+            this.ResetState.Text = "Reset";
+            this.ResetState.UseVisualStyleBackColor = true;
+            this.ResetState.Click += new System.EventHandler(this.OnResetClick);
+            // 
+            // PlayerL
+            // 
+            this.PlayerL.AutoSize = true;
+            this.PlayerL.Location = new System.Drawing.Point(326, 41);
+            this.PlayerL.Name = "PlayerL";
+            this.PlayerL.Size = new System.Drawing.Size(48, 17);
+            this.PlayerL.TabIndex = 3;
+            this.PlayerL.Text = "--------";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PlayerL);
+            this.Controls.Add(this.ResetState);
+            this.Controls.Add(this.DebugL);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label DebugL;
+        private System.Windows.Forms.Button ResetState;
+        private System.Windows.Forms.Label PlayerL;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace TicTacToe
+{
+   
+
+    public partial class Form1 : Form
+    { 
+        
+    #region CONSTANTS
+    const int CELL = 89;
+        const int CellCount = 3;
+       
+    #endregion
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            m_CC = m_CA;
+            PlayerL.Text = "Player 1: Circle";
+        }
+
+        Pen cross = new Pen(Color.Red, 2);
+        Pen circle = new Pen(Color.Purple, 2);
+        bool[,] m_CA = new bool[CellCount, CellCount];
+        bool[,] m_CB = new bool[CellCount, CellCount];
+        bool[,] m_CC; // current ( active ) CellArray n'te Generation pointer zum umschalten 
+        int PlayerStateCircle = 1; // 1 = priority
+        int PlayerStateRectangle = 0;
+
+        private void OnPanelPaint(object sender, PaintEventArgs e)
+        {
+            DrawGrid(e.Graphics);
+            PlayerState(PlayerStateCircle,PlayerStateRectangle,e); 
+        }
+
+        private void PlayerState(int PSC, int PSR, PaintEventArgs e)
+        {
+            int stateC = PSC;
+            int stateR = PSR;
+            if(stateC == 1)
+            {
+                DrawCircle(e.Graphics);
+                stateR = 1;
+            }
+            else if(stateR == 1)
+            {
+                DrawCross(e.Graphics);
+                stateC = 1;
+            }
+        }
+
+        void DrawGrid(Graphics gr)
+        {
+            Pen pen = new Pen(Color.DarkBlue, 3);
+            int x, y;
+            for(int i = 0; i < 5; i++)
+            {
+                x = i * CELL;
+                gr.DrawLine(pen, x, 0, x, CellCount * CELL);
+
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                y = j * CELL;
+                gr.DrawLine(pen, CellCount * CELL, y, 0, y);
+            }
+        }
+       
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            int x = MousePosition.X;
+            int y = MousePosition.Y;
+            PlayerL.Text = "Player 1: Circle";
+            if (e.Button == MouseButtons.Left && PlayerStateCircle == 1)
+            {
+                x = e.X;
+                y = e.Y;
+                x = x / CELL;
+                y = y / CELL;
+                DebugL.Text = x + " " + y;
+                ToggleDrawCross(x,y);
+                PlayerStateRectangle = 1;
+                PlayerStateCircle = 0;
+                PlayerL.Text = "Player 2: Rectangle";
+            }
+            else if(e.Button == MouseButtons.Left && PlayerStateRectangle == 1)
+            {
+                x = e.X;
+                y = e.Y;
+                x = x / CELL;
+                y = y / CELL;
+                DebugL.Text = x + " " + y;
+                ToggleDrawCircle(x,y);
+                PlayerStateRectangle = 0;
+                PlayerStateCircle = 1;
+            }
+            panel.Invalidate();
+        }
+
+        void ToggleDrawCircle(int x, int y)
+        {
+            m_CC[x, y] = true;
+        }
+        void ToggleDrawCross(int x, int y)
+        {
+           m_CC[x,y] =  m_CB[x, y] = true;
+            
+        }
+
+        void DrawCross(Graphics gr)
+        {
+            int x, y;
+            for (int i = 0; i < CellCount; i++)
+            {
+                for (int j = 0; j < CellCount; j++)
+                {
+                    x = i * CELL;
+                    y = j * CELL;
+
+                    if (m_CC[i, j])
+                    {    // if true = alive 
+                       // gr.FillRectangle(br, x, y, CELL_SIZE - 2 * C_M, CELL_SIZE - 2 * C_M);
+                        gr.DrawRectangle(cross, x, y, CELL-2 * 2, CELL-2*2);
+                    }
+
+                }
+            }
+        }
+
+        void DrawCircle(Graphics gr)
+        {
+            int x, y;
+            for (int i = 0; i < CellCount; i++)
+            {
+                for (int j = 0; j < CellCount; j++)
+                {
+                    x = i * CELL;
+                    y = j * CELL;
+
+                    if (m_CC[i, j])
+                    {    // if true = alive 
+                         // gr.FillRectangle(br, x, y, CELL_SIZE - 2 * C_M, CELL_SIZE - 2 * C_M);
+                        gr.DrawEllipse(circle, x, y, CELL-2*2, CELL-2*2);
+                    }
+
+                }
+            }
+            
+        }
+
+        private void OnResetClick(object sender, EventArgs e)
+        {
+            PlayerStateCircle = 1;
+            PlayerStateRectangle = 0;
+           for(int i = 0; i < CellCount; i++)
+            {
+                for(int j = 0; j < CellCount; j++)
+                {
+                    m_CC[i, j] = false;
+                }
+            }
+            panel.Invalidate();
+        }
+    }
+
+    // ToDO:
+    //PlayerState mit der Form Verbinden
+}
